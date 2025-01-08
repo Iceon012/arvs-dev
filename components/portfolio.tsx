3874d43c14370aa614d17c1781d8a483d63bfced
titovin <arvin@titosolutions.ph>
2025-01-09 02:49:22 +0800

    first commit

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -80,7 +80,7 @@ const Portfolio = () => {
               animate={{ opacity: 1 }}
               transition={{ delay: 0.5 }}
             >
-              Web Developer
+              Web Developer / Back End Developer
             </motion.p>
             <div className="flex justify-center space-x-4">
               <motion.a
